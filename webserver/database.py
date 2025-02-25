7f174892c76725e1812bf22f79409e1319b23473
Johannes044 <168712126+Johannes044@users.noreply.github.com>
2025-02-25 12:33:28 +0100

    Update database.py 2

--- webserver/database.py
+++ webserver/database.py
@@ -3,46 +3,28 @@ from flask_cors import CORS
 import redis
 import json
 
-
 app = Flask(__name__)
 CORS(app)
 
-# change this to connect to your redis server
-# ===============================================
-redis_server = redis.Redis("localhost", decode_responses=True, charset="unicode_escape")
-# ===============================================
+redis_server = redis.Redis(host='localhost', port=6379, decode_responses=True, charset="unicode_escape")
 
 @app.route('/drone', methods=['POST'])
 def drone():
-    drone = request.get_json() #* Tar emot en json packet från drönaren.
-    drone_IP = request.remote_addr #* Hämtar dränarens IP.
-    drone_ID = drone['id']
+    drone = request.get_json()
+    droneIP = request.remote_addr
+    droneID = drone['id']
     drone_longitude = drone['longitude']
     drone_latitude = drone['latitude']
     drone_status = drone['status']
-    # Get the infomation of the drone in the request, and update the information in Redis database
-    # Data that need to be stored in the database: 
-    # Drone ID, logitude of the drone, latitude of the drone, drone's IP address, the status of the drone
-    # Note that you need to store the metioned infomation for all drones in Redis, think carefully how to store them
-    # =========================================================================================
-
-    #* Sätter upp drönar information i ett Dictionarie (nyckel-värde par).
-    drone_data = {
-        "id": drone_ID,
-        "longitude": drone_longitude,
-        "latitude": drone_latitude,
-        "ip_address": drone_IP,
-        "status": drone_status
-    }
-
-    #* 
-    redis_server.set(drone_ID, json.dumps(drone_data))
-
-
-    # =======================================================================================
-    return "Drone data updated successfully", 200
+    
+    drones = redis_server.smembers('drones')
+    if droneID not in drones:
+        redis_server.sadd('drones', droneID)
+    
+    droneDATA = {'ip': droneIP, 'longitude': drone_longitude, 'latitude': drone_latitude, 'status': drone_status}
+    redis_server.hset(droneID, mapping=droneDATA)
+    
+    return 'Get data'
 
 if __name__ == "__main__":
-
-
     app.run(debug=True, host='0.0.0.0', port='5001')
