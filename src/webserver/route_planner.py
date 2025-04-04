22c8d418909819d375f0e449306380bf0b6f8de4
Johannes Persson <johannesmail04@gmail.com>
2025-04-04 20:09:29 +0200

    added logging to route_planner

--- src/webserver/route_planner.py
+++ src/webserver/route_planner.py
@@ -43,9 +43,11 @@ def route_planner():
     to_location = geolocator.geocode(ToAddress + region, timeout=None)
     
     if from_location is None:
-        return 'Departure address not found, please input a correct address'
+        logging.error("Departure from_address not found, please input a correct address")
+        return 'Departure from_address not found, please input a correct address'
     elif to_location is None:
-        return 'Destination address not found, please input a correct address'
+        logging.error('Destination to_address not found, please input a correct address')
+        return 'Destination to_address not found, please input a correct address'
     
     coords = {'from': (from_location.longitude, from_location.latitude),
               'to': (to_location.longitude, to_location.latitude)}
@@ -56,6 +58,7 @@ def route_planner():
     droneAvailable = None
     for drone in drones:
         droneData = redis_server.hgetall(drone)
+        logging.debug(f"Drone data from {droneData['id']} have been access!")
         if droneData['status'] == 'idle':
             droneAvailable = drone
             coords['current'] = (droneData['longitude'], droneData['latitude'])
@@ -67,6 +70,7 @@ def route_planner():
     DRONE_IP = redis_server.hget(droneAvailable, 'ip')
     DRONE_URL = f'http://{DRONE_IP}:5000'
     send_request(DRONE_URL, coords)
+    logging.debug('Got address and sent request to the drone')
     return 'Got address and sent request to the drone'
 
     try:
