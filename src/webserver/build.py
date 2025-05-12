4da02aef0eb61af898ed7976bab460c441121c54
ellasvensson <ella.eaf.svensson@hotmail.com>
2025-05-12 16:54:34 +0200

    malmdlkad

--- src/webserver/build.py
+++ src/webserver/build.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, request, redirect, url_for
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 import redis
