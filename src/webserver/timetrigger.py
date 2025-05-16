6ebe9e21392ad93bb44e0d74d46d5a29167585a3
Edvin Sternerup <edvin.sternerup@gmail.com>
2025-05-16 15:26:13 +0200

    småändring igen

--- src/webserver/timetrigger.py
+++ src/webserver/timetrigger.py
@@ -4,9 +4,6 @@ import requests
 while True:
     time.sleep(5)
     with requests.Session() as session:
-        resp = session.post("http://localhost/sender", {})
+        resp = session.post("http://localhost:1339/sender", {})
         print("hejsan hoppsan")
 
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='1339')
